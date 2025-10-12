e4014b91415c38bc3a7fa2199b2602d8988df39d
Julio Corbalán Moreno <jcorbalanm@mailbox.org>
2025-10-12 02:01:34 +0200

    Update nixos-config path

--- configuration.nix
+++ configuration.nix
@@ -23,7 +23,7 @@
 
  
   nix.nixPath = [
-    "nixos-config=${config.users.users.jcorbalan.home}/nixos-config/configuration.nix"
+    "nixos-config=${config.users.users.jcorbalan.home}/Repositorios/nixos-config/configuration.nix"
     "nixpkgs=/nix/var/nix/profiles/per-user/root/channels/nixos"
     "/nix/var/nix/profiles/per-user/root/channels"
   ];
@@ -218,7 +218,7 @@
         upgrade = "sudo nixos-rebuild switch --upgrade";
         change-mac = "sudo bash -c 'sudo tailscale down; systemctl stop NetworkManager; macchanger -r wlp4s0; sudo systemctl start NetworkManager'";
         restore-mac = "sudo bash -c 'sudo tailscale down; systemctl stop NetworkManager; macchanger -p wlp4s0; sudo systemctl start NetworkManager'";
-        rebuild = "sudo bash -c 'nano /home/jcorbalan/nixos-config/configuration.nix; nixos-rebuild switch'";
+        rebuild = "sudo bash -c 'nano /home/jcorbalan/Repositorios/nixos-config/configuration.nix; nixos-rebuild switch'";
 	      disable-battery-conservation = "sudo /home/jcorbalan/lenovo-legion5-15arh05-scripts/BatteryConservation/disable_battery_conservation.sh";
 	      enable-battery-conservation = "sudo /home/jcorbalan/lenovo-legion5-15arh05-scripts/BatteryConservation/enable_battery_conservation.sh";
       };
