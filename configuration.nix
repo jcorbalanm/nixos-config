881c5b7069b90620862217d514c733682a28f710
Julio Corbalán Moreno <jcorbalan@mailbox.org>
2026-05-07 13:12:32 +0200

    Change stdenv.hostPlatform.system.stateVersion into system.stateVersion

--- configuration.nix
+++ configuration.nix
@@ -477,7 +477,7 @@
   # and migrated your data accordingly.
   #
   # For more information, see `man configuration.nix` or https://nixos.org/manual/nixos/stable/options#opt-system.stateVersion .
-  stdenv.hostPlatform.system.stateVersion = "24.05"; # Did you read the comment?
+  system.stateVersion = "24.05"; # Did you read the comment?
 
 }
 
